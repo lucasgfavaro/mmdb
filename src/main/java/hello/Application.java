1008f0f79a885df81a8c12434a382f2151e56943
Lucas Favaro <lucasgracianofavaro@gmail.com>
2017-07-17 22:08:00 -0300

    Add springboot plugin to run app

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -2,12 +2,14 @@ package hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @EnableAutoConfiguration
+@SpringBootApplication
 public class Application {
 
     @RequestMapping("/")
